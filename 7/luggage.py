397b2d0bb2a8101052d290a691d85e7ad8b17d64
Toby Waite <toby.waite@gmail.com>
2020-12-07 23:06:50 -0700

    cleaned up day 7

--- 7/luggage.py
+++ 7/luggage.py
@@ -4,57 +4,54 @@ from functools import reduce
 class Bag():
     def __init__(self, input_line):
 
-        bag_color, children_input = input_line.split(" bags contain ")
+        def parse(line):
+            bag_pattern = r"(?P<color>\w+ \w+) bags contain (?P<children_string>.*)\."
+            child_pattern= r"(?P<count>\d+) (?P<color>\w+ \w+) bags?"
+            
+            bag_color, children_string = re.match(bag_pattern, line).groups()
+            children = [(int(count), color) for (count, color) in re.findall(child_pattern, children_string)]
 
-        self.color = bag_color
-        self.children = dict()
-        self.total_children = None
+            return bag_color, children
 
-        for child in children_input.split(","):
-            child_regex = r"(?P<count>\d+) (?P<color>\w+ \w+) bag"
-            match = re.match(child_regex, child.strip())
+        self.color, self.children = parse(input_line)
+        self.count = None
 
-            if match:
-                self.children[match.groupdict()["color"]] = int(match.groupdict()["count"])
-                print("{child} -- {matches}".format(child=child, matches=match.groupdict()))
-        
-
-    def child_count(self, bagLookup):
-        if self.total_children:
-            return self.total_children
-        elif len(self.children)==0:
-            return 0
-        else:
-            total = sum([(bagsLookup[color].child_count(bagsLookup) + 1) * count for (color, count) in self.children.items()])
-            self.total_children = total
-            return total
-    
     def __repr__(self):
         return "{color} (total: {total}): {children}".format(color=self.color, total=self.total_children, children=self.children)
-
+    
+    def countBags(self, bagGraph):
+        if len(self.children) == 0:
+            return 0
+        else:
+            if self.count:
+                return self.count
+            self.count = sum([count * (1 + bagGraph[child].countBags(bagGraph)) for (count, child) in self.children])
+            return self.count
+        
 
 with open("input.txt") as f:
     input = [line.strip("\n") for line in f.readlines()]
 
 bags = [Bag(line) for line in input]
-bagsLookup = {bag.color: bag for bag in bags}
+graph = {bag.color: bag for bag in bags}
 
-solution = set()
 starting_bag = 'shiny gold'
+
+solution1 = set()
 possible_bags = set([starting_bag, ])
 
 while len(possible_bags) > 0:
     possible_bag = possible_bags.pop()
-    solution.add(possible_bag)
+    solution1.add(possible_bag)
 
-    for color in bagsLookup.keys():
-        if color not in solution and possible_bag in bagsLookup[color].children:
-            possible_bags.add(color);
+    for bag in graph:
+        if bag not in solution1 and possible_bag in [color for (count, color) in graph[bag].children]:
+            possible_bags.add(bag);
 
-solution.remove(starting_bag)
+solution1.remove(starting_bag)
 
-print("Possible bags ({})".format(len(solution)))
+print("Possible bags ({})".format(len(solution1)))
 
-count = bagsLookup[starting_bag].child_count(bagsLookup);
+solution2 = graph[starting_bag].countBags(graph);
 
-print("Total contained bags in {}: {}".format(starting_bag, count))
+print("Total contained bags in {}: {}".format(starting_bag, solution2))
